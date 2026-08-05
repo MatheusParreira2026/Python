128d59f534fd9f01f9013376c95544d8c5b711c4
MatheusParreira2026 <matheusparreira.programacao@gmail.com>
2026-08-05 10:41:09 -0300

    Update Desafio_04.py

--- Mundo_01_Fundamentos/Aula_06_Tipos_Primitivos_e_Saida_de_Dados/Desafio_04.py
+++ Mundo_01_Fundamentos/Aula_06_Tipos_Primitivos_e_Saida_de_Dados/Desafio_04.py
@@ -6,29 +6,38 @@ primitivo e todas as informações sobre ele.
 
 entrada = input("Digite algo: ")
 
-print(f'{entrada} é uma string e está escrita somente com letras maiúsculas?')
+print(f'\n{entrada} possui letras e todas elas estão em maiúsculas?')
 print('Resposta:', entrada.isupper())
 
-print(f'{entrada} é uma string e está escrita somente com letras maiúsculas?')
-print('Resposta:', entrada.isupper())
+print(f'\n{entrada} possui letras e todas elas estão em minúsculas?')
+print('Resposta:', entrada.islower())
 
-print(f'{entrada} é uma string e está escrita somente com letras maiúsculas?')
-print('Resposta:', entrada.isupper())
+print(f'\n{entrada} contém somente letras?')
+print('Resposta:', entrada.isalpha())
 
-print(f'{entrada} é uma string e está escrita somente com letras maiúsculas?')
-print('Resposta:', entrada.isupper())
+print(f'\n{entrada} contém somente dígitos?')
+print('Resposta:', entrada.isdigit())
 
-print(f'{entrada} é uma string e está escrita somente com letras maiúsculas?')
-print('Resposta:', entrada.isupper())
+print(f'\n{entrada} contém somente caracteres decimais?')
+print('Resposta:', entrada.isdecimal())
 
-print(f'{entrada} é uma string e está escrita somente com letras maiúsculas?')
-print('Resposta:', entrada.isupper())
+print(f'\n{entrada} contém somente caracteres numéricos?')
+print('Resposta:', entrada.isnumeric())
 
-print(f'{entrada} é uma string e está escrita somente com letras maiúsculas?')
-print('Resposta:', entrada.isupper())
+print(f'\n{entrada} contém somente letras e/ou números?')
+print('Resposta:', entrada.isalnum())
 
-print(f'{entrada} é uma string e está escrita somente com letras maiúsculas?')
-print('Resposta:', entrada.isupper())
+print(f'\n{entrada} contém somente espaços?')
+print('Resposta:', entrada.isspace())
 
-print(f'{entrada} é uma string e está escrita somente com letras maiúsculas?')
-print('Resposta:', entrada.isupper())
+print(f'\n{entrada} está no formato de título?')
+print('Resposta:', entrada.istitle())
+
+print(f'\n{entrada} contém somente caracteres ASCII?')
+print('Resposta:', entrada.isascii())
+
+print(f'\n{entrada} pode ser usado como identificador Python?')
+print('Resposta:', entrada.isidentifier())
+
+print(f'\n{entrada} tem todos os caracteres imprimíveis?')
+print('Resposta:', entrada.isprintable())
