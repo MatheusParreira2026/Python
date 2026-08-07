e59490fe26f3a4cad2b2084091e9942e7b4ddc1d
MatheusParreira2026 <matheusparreira.programacao@gmail.com>
2026-08-07 13:05:15 -0300

    novo

--- Mundo_01_Fundamentos/Aula_07_Operadores_Aritmeticos/Desafio_009.py
+++ Mundo_01_Fundamentos/Aula_07_Operadores_Aritmeticos/Desafio_009.py
@@ -3,25 +3,45 @@ Faça um programa que leia um número inteiro qualquer
 e mostre na tela sua tabuada.
 '''
 
-n = int(input('Digite um número inteiro: '))
+# n = int(input('Digite um número inteiro: '))
+#
+# n1 = n * 1
+# n2 = n * 2
+# n3 = n * 3
+# n4 = n * 4
+# n5 = n * 5
+# n6 = n * 6
+# n7 = n * 7
+# n8 = n * 8
+# n9 = n * 9
+# n10 = n * 10
+#
+# print(f'>>>TABUADA DO NÚMERO {n}<<<')
+# print(f'{n} x 1 = {n1}')
+# print(f'{n} x 2 = {n2}')
+# print(f'{n} x 3 = {n3}')
+# print(f'{n} x 4 = {n4}')
+# print(f'{n} x 5 = {n5}')
+# print(f'{n} x 6 = {n6}')
+# print(f'{n} x 7 = {n7}')
+# print(f'{n} x 8 = {n8}')
+# print(f'{n} x 9 = {n9}')
+# print(f'{n} x 10 = {n10}')
 
-n1 = n * 1
-n2 = n * 2
-n3 = n * 3
-n4 = n * 4
-n5 = n * 5
-n6 = n * 6
-n7 = n * 7
-n8 = n * 8
-n9 = n * 9
+'''
+Alternativa
+'''
 
-print(f'>>>TABUADA DO NÚMERO {n}<<<')
-print(f'{n} x 1 = {n1}')
-print(f'{n} x 2 = {n2}')
-print(f'{n} x 3 = {n3}')
-print(f'{n} x 4 = {n4}')
-print(f'{n} x 5 = {n5}')
-print(f'{n} x 6 = {n6}')
-print(f'{n} x 7 = {n7}')
-print(f'{n} x 8 = {n8}')
-print(f'{n} x 9 = {n9}')
+num = int(input('Digite um número para ver sua tabuada: '))
+print('-' * 20)
+print('{} x {:2} = {}'.format(num, 1, num * 1))
+print('{} x {:2} = {}'.format(num, 2, num * 2))
+print('{} x {:2} = {}'.format(num, 3, num * 3))
+print('{} x {:2} = {}'.format(num, 4, num * 4))
+print('{} x {:2} = {}'.format(num, 5, num * 5))
+print('{} x {:2} = {}'.format(num, 6, num * 6))
+print('{} x {:2} = {}'.format(num, 7, num * 7))
+print('{} x {:2} = {}'.format(num, 8, num * 8))
+print('{} x {:2} = {}'.format(num, 9, num * 9))
+print('{} x {:2} = {}'.format(num, 10, num * 10))
+print('-' * 20)
--- Mundo_01_Fundamentos/Aula_07_Operadores_Aritmeticos/Desafio_010.py
+++ Mundo_01_Fundamentos/Aula_07_Operadores_Aritmeticos/Desafio_010.py
@@ -4,8 +4,44 @@ uma pessoa tem na carteira e mostre quantos
 dólares ela pode comprar.
 Considere US$1,00 = R$3,27
 '''
+from idlelib import replace
 
-real =  float(input('Digite o valor total do seu dinheiro em reais: '))
-conversor = real * 3.27
-print(f'R${real:.2f} convertido em dólares é igual a: US${conversor:.2f}')
+# real =  float(input('Digite o valor total do seu dinheiro em reais: R$ '))
+# conversor = real / 3.27
+# print(f'R${real:.2f} convertido em dólares é igual a: US${conversor:.2f}')
 
+'''Alternativo'''
+
+import requests
+
+real = float(input("Digite um valor monetário em reais: "))
+
+resposta = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL")
+
+dados = resposta.json()
+
+cotacao = float(dados['USDBRL']["bid"])
+
+conversor = real / cotacao
+
+real_formatado = f"{real:.2f}".replace(".", ",")
+
+valor_formatado = f"{conversor:.2f}".replace(".", ",")
+
+print(f'O valor de R$ {real_formatado} em dólares é igual a: US$ {valor_formatado}')
+
+'''
+Exemplo
+'''
+# dados = {
+#     "USD": {
+#         "nome": "Dólar",
+#         "valor": "5.43"
+#     }
+# }
+#
+# '''
+# Como chegar até "5.43"?
+#
+# '''
+# print(dados["USD"]["valor"])
