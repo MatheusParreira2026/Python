8e54eed39f9e0914af0c4f7af774ec25c8f2023b
MatheusParreira2026 <matheusparreira.programacao@gmail.com>
2026-08-18 12:34:47 -0300

    Update Desafio_14_Extra.py

--- Mundo_01_Fundamentos/Aula_07_Operadores_Aritmeticos/Desafio_14_Extra.py
+++ Mundo_01_Fundamentos/Aula_07_Operadores_Aritmeticos/Desafio_14_Extra.py
@@ -6,5 +6,3 @@ e converta para ºF.
 celsius = float(input('Dgite a temperatura em graus Celsius: '))
 celsius_para_fahrenheit = celsius * 9 / 5 + 32
 print(f'{celsius:.2f}ºC equivale a {celsius_para_fahrenheit:.2f}ºF')
-
-
