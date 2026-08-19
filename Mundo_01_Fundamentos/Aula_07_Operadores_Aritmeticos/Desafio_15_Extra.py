ec6138107b07e7320ea06375f1d8d456e798135f
MatheusParreira2026 <matheusparreira.programacao@gmail.com>
2026-08-19 12:01:11 -0300

    Update Desafio_15_Extra.py

--- Mundo_01_Fundamentos/Aula_07_Operadores_Aritmeticos/Desafio_15_Extra.py
+++ Mundo_01_Fundamentos/Aula_07_Operadores_Aritmeticos/Desafio_15_Extra.py
@@ -5,11 +5,22 @@ alugado. Calcule o preço a pagar, sabendo que o carro custa
 R$60 por dia e R$0,15 por Km rodado.
 '''
 
+# km = float(input('Quantos quilômetros o seu carro percorreu? '))
+# dias = int(input('Quantos dias o senhor(a) usou o carro? '))
+#
+# preco_por_dia = dias * 60
+# preco_por_km = km * 0.15
+# preco_total = preco_por_dia + preco_por_km
+#
+# print(f'O valor total a ser pago é de R$ {preco_total:.2f}')
+
+'''
+Alternativa
+'''
+
 km = float(input('Quantos quilômetros o seu carro percorreu? '))
 dias = int(input('Quantos dias o senhor(a) usou o carro? '))
 
-preco_por_dia = dias * 60
-preco_por_km = km * 0.15
-preco_total = preco_por_dia + preco_por_km
+preco_total = (dias * 60) + (km * 0.15)
 
-print(f'O valor total a ser pago é de R$ {preco_total:.2f}')
+print(f'O valor total a ser pago é de R${preco_total:.2f}')
