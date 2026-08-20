13fe7a09085e36243432ccf771fb0f70b06cca4c
MatheusParreira2026 <matheusparreira.programacao@gmail.com>
2026-08-20 16:32:50 -0300

    Create Aula_08_Exemplos_em_Aula.py

---
+++ Mundo_01_Fundamentos/Aula_08_Utilizando_Modulos/Aula_08_Exemplos_em_Aula.py
@@ -0,0 +1,84 @@
+'''
+Exemplo 01
+Importando a biblioteca "math"
+'''
+# import math
+# num = int(input('Digite um número: '))
+# raiz = math.sqrt(num)
+# print(f'A raiz de {num} é igual a {raiz}')
+
+'''
+Exemplo 02
+Arredondando o número para cima utilizando o "ceil"
+'''
+# import math
+# num = int(input('Digite um número: '))
+# raiz = math.sqrt(num)
+# print(f'A raiz de {num} é igual a {math.ceil(raiz)}')
+
+'''
+Exemplo 03
+Arredondando o número para baixo utiliznado o "floor"
+'''
+# import math
+# num = int(input('Digite um número: '))
+# raiz = math.sqrt(num)
+# print(f'A raiz de {num} é igual a {math.floor(raiz)}')
+
+'''
+Exemplo 04
+Arrendondo com duas casas decimais
+'''
+# import math
+# num = int(input('Digite um número: '))
+# raiz = math.sqrt(num)
+# print(f'A raiz de {num} é igual a {raiz:.2f}')
+
+'''
+Exemplo 05
+Importando somente "sqrt" da biblioteca "math"
+'''
+# from math import sqrt
+# num = int(input('Digite um número: '))
+# raiz = sqrt(num)
+# print(f'A raiz de {num} é igual a {raiz:.2f}')
+
+'''
+Exemplo 06
+Importando somente "sqrt" e "floor" da biblioteca "math"
+'''
+# from math import sqrt, floor
+# num = int(input('Digite um número: '))
+# raiz = sqrt(num)
+# print(f'A raiz de {num} é igual a {floor(raiz):.2f}')
+
+'''
+Exemplo 07
+Importando a biblioteca "random"
+'''
+# import random
+# num = random.random()
+# print(num)
+
+'''
+Exemplo 08
+Randomizando um número inteiro utilizando a biblioteca "random"
+'''
+# import random
+# num = random.randint(1, 10)
+# print(num)
+
+'''
+Exemplo 09
+Aperte "ctrl" + "espaço" depois do "import" para ver todas as
+bibliotecas padrão do Python 
+'''
+# import
+
+'''
+Exemplo 10
+Importando a biblioteca externa "emoji" utlizando  o "pip install emoji"
+no "Terminal"
+'''
+import emoji
+print(emoji.emojize('Olá, :earth_americas:!', language='alias'))
