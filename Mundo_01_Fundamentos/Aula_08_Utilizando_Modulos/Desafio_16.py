6385ca2acadffa211b7dd3f30949f2134030ae9c
MatheusParreira2026 <matheusparreira.programacao@gmail.com>
2026-08-25 12:17:41 -0300

    Novo

--- Mundo_01_Fundamentos/Aula_08_Utilizando_Modulos/Desafio_16.py
+++ Mundo_01_Fundamentos/Aula_08_Utilizando_Modulos/Desafio_16.py
@@ -6,8 +6,20 @@ Por exemplo, se o usuário digitar o valor 6.127, o programa deve exibir apenas 
 DICA:O aluno deve examinar as funções disponíveis dentro da classe do módulo math,
 que foi o módulo importado durante a aula para realizar manipulações numéricas.
 '''
-import math
+# import math
+# numero = float(input("Digite um número: "))
+# porcao_inteira = math.trunc(numero)
+# print(f'A porção inteira do número {numero} é {porcao_inteira}')
 
-numero = float(input("Digite um número: "))
-porcao_inteira = math.trunc(numero)
-print(f'A porção inteira do número {numero} é {porcao_inteira}')
+'''
+Alternativa 01
+'''
+# from math import trunc
+# numero = float(input("Digite um número: "))
+# print(f'A porção inteira do número {numero} é {trunc(numero)}')
+
+'''
+Alternativa 02
+'''
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, int(num)))
--- Mundo_01_Fundamentos/Aula_08_Utilizando_Modulos/Desafio_17.py
+++ Mundo_01_Fundamentos/Aula_08_Utilizando_Modulos/Desafio_17.py
@@ -5,9 +5,26 @@ adjacente de um triângulo retângulo e calcule e mostre o comprimento da hipote
 DICA: é possível resolver esse desafio de forma simples utilizando recursos de módulos matemáticos,
 aplicando o princípio de que o quadrado da hipotenusa é igual à soma dos quadrados dos catetos.
 '''
-import math
 
+# cateto_oposto = float(input('Comprimento do cateto oposto: '))
+# cateto_adjacente = float(input("Comprimento do cateto adjacente: "))
+# hipotenusa = (cateto_oposto ** 2 + cateto_adjacente ** 2) ** (1/2)
+# print(f'A hipotenusa é igual {hipotenusa:.2f}')
+
+
+'''
+Alternativa 01
+'''
+# import math
+# cateto_oposto = float(input('Comprimento do cateto oposto: '))
+# cateto_adjacente = float(input("Comprimento do cateto adjacente: "))
+# hipotenusa = math.sqrt((cateto_oposto ** 2) + (cateto_adjacente ** 2))
+# print(f'A hipotenusa é igual {hipotenusa:.2f}')
+
+'''
+Alternativa 02
+'''
+from math import hypot
 cateto_oposto = float(input('Comprimento do cateto oposto: '))
 cateto_adjacente = float(input("Comprimento do cateto adjacente: "))
-hipotenusa = math.sqrt((cateto_oposto ** 2) + (cateto_adjacente ** 2))
-print(f'A hipotenusa é igual {hipotenusa:.2f}')
+print('A hipotenusa é igual a {:.2f}'.format(hypot(cateto_oposto, cateto_adjacente)))
