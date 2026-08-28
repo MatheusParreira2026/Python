b358b4b7c85067c202eccca6a7e27b98eb389ff1
MatheusParreira2026 <matheusparreira.programacao@gmail.com>
2026-08-28 15:49:42 -0300

    Update Desafio_29.py

--- Mundo_01_Fundamentos/Aula_10_Condicoes/Desafio_29.py
+++ Mundo_01_Fundamentos/Aula_10_Condicoes/Desafio_29.py
@@ -14,22 +14,21 @@ Se o carro estiver a 80 km/h ou menos, o programa não deve mostrar
 nenhuma mensagem de multa.
 '''
 
-velocidade = float(input('Informe a velocidade atual do carro: '))
-
-if velocidade > 80:
-    excesso_de_velocidade = (velocidade - 80) * 7
-    print(f'Você foi multado! O valor da sua multa é de: R$ {excesso_de_velocidade:.2f}.')
-else:
-    print(f'Sua velocidade é de {velocidade:.2f}km/h. Essa velocidade está dentro do limite.')
+# velocidade = float(input('Informe a velocidade atual do carro: '))
+#
+# if velocidade > 80:
+#     excesso_de_velocidade = (velocidade - 80) * 7
+#     print(f'Você foi multado! O valor da sua multa é de: R$ {excesso_de_velocidade:.2f}.')
+# else:
+#     print(f'Sua velocidade é de {velocidade:.2f}km/h. Essa velocidade está dentro do limite.')
 
 '''
 Alternativa feita pelo professor
 '''
-# velocidade = float(input('Qual é a velocidade atual do carro? '))
-# if velocidade > 80:
-#     print('Multado! Você excedeu o limite permitido de 80km/h')
-#     multa = (velocidade - 80) * 7
-#     print(f'Você deve pagar uma multa de R${multa:.2f}')
-#
-#     print('Tenha um bom dia! Dirija com segurança')
-#
+velocidade = float(input('Qual é a velocidade atual do carro? '))
+if velocidade > 80:
+    print('Multado! Você excedeu o limite permitido de 80km/h')
+    multa = (velocidade - 80) * 7
+    print(f'Você deve pagar uma multa de R${multa:.2f}')
+
+    print('Tenha um bom dia! Dirija com segurança')
