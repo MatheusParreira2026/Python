6b7b208253841dcecb63361ab38718c959034489
MatheusParreira2026 <matheusparreira.programacao@gmail.com>
2026-08-31 15:55:47 -0300

    Novo

--- Mundo_01_Fundamentos/Aula_10_Condicoes/Desafio_32.py
+++ Mundo_01_Fundamentos/Aula_10_Condicoes/Desafio_32.py
@@ -19,15 +19,32 @@ em Python para exibir a resposta correta.
 # elif ano % 4 == 0:
 #     print(f'O ano {ano} é um ano bissexto.')
 
+# from datetime import date
+# ano = int(input('Digite um ano qualquer ou 0 para o ano atual: '))
+# if ano == 0:
+#     ano = date.today().year
+# if ano % 400 == 0:
+#     print(f'O ano {ano} é um ano bissexto.')
+# elif ano % 100 == 0:
+#     print(f'O ano {ano} não é um ano bissexto.')
+# elif ano % 4 == 0:
+#     print(f'O ano {ano} é um ano bissexto.')
+# else:
+#     print(f'O ano {ano} não é um ano bissexto.')
+
+'''
+Exemplo feito pelo professor
+
+O código utiliza a biblioteca datetime para obter o ano atual 
+caso o usuário digite 0, e aplica a lógica matemática necessária
+para identificar anos bissextos (divisível por 4, mas não por 
+100, a menos que também seja divisível por 400).
+'''
 from datetime import date
-ano = int(input('Digite um ano qualquer ou 0 para o ano atual: '))
+ano = int(input("Que ano quer analisar? Coloque 0 para analisar o ano atual: "))
 if ano == 0:
     ano = date.today().year
-if ano % 400 == 0:
-    print(f'O ano {ano} é um ano bissexto.')
-elif ano % 100 == 0:
-    print(f'O ano {ano} não é um ano bissexto.')
-elif ano % 4 == 0:
-    print(f'O ano {ano} é um ano bissexto.')
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print(f"O ano {ano} é BISSEXTO")
 else:
-    print(f'O ano {ano} não é um ano bissexto.')
+    print(f"O ano {ano} NÃO é bissexto")
--- Mundo_01_Fundamentos/Aula_10_Condicoes/Desafio_33.py
+++ Mundo_01_Fundamentos/Aula_10_Condicoes/Desafio_33.py
@@ -8,22 +8,49 @@ entre múltiplas variáveis e o uso de blocos if e else para
 determinar condições de desigualdade.
 '''
 
-while True:
-    n1 = float(input('Digite o primeiro número: '))
-    n2 = float(input('Digite o segundo número: '))
-    n3 = float(input('Digite o terceiro número: '))
 
-    if n1 > n2 and n1 > n3:
-        print(f'O número {n1} é maior que {n2} e {n3}.')
-    elif n2 > n1 and n2 > n3:
-        print(f'O número {n2} é maior que {n1} e {n3}')
-    else:
-        print(f'O numero {n3} é maior que {n1} e {n2}')
+# n1 = float(input('Digite o primeiro número: '))
+# n2 = float(input('Digite o segundo número: '))
+# n3 = float(input('Digite o terceiro número: '))
+#
+# if n1 > n2 and n1 > n3:
+#     print(f'O número {n1} é maior que {n2} e {n3}.')
+# elif n2 > n1 and n2 > n3:
+#     print(f'O número {n2} é maior que {n1} e {n3}')
+# else:
+#     print(f'O numero {n3} é maior que {n1} e {n2}')
+#
+# if n1 < n2 and n1 < n3:
+#     print(f'O número {n1} é menor que {n2} e {n3}')
+# elif n2 < n1 and n2 < n3:
+#     print(f'O número {n2} é menor que {n1} e {n3}')
+# else:
+#     print(f'O número {n3} é menor que {n1} e {n2}')
 
-    if n1 < n2 and n1 < n3:
-        print(f'O número {n1} é menor que {n2} e {n3}')
-    elif n2 < n1 and n2 < n3:
-        print(f'O número {n2} é menor que {n1} e {n3}')
-    else:
-        print(f'O número {n3} é menor que {n1} e {n2}')
+'''
+Exercício feito pelo professor
+Abaixo apresento o código sugerido (3:27), que otimiza a lógica
+assumindo inicialmente que o primeiro número é tanto o maior
+quanto o menor, realizando testes apenas com os outros dois:
+'''
 
+a = int(input('Primeiro valor: '))
+b = int(input('Segundo valor: '))
+c = int(input('Terceiro valor: '))
+
+# Verificando quem é menor
+menor = a
+if b < a and b < c:
+    menor = b
+if c < a and c < b:
+    menor = c
+
+# Verificando quem é maior
+maior = a
+if b > a and b > c:
+    maior = b
+if c > a and c > b:
+    maior = c
+
+print(f'O menor valor digitado foi {menor}')
+print(f'O maior valor digitado foi {maior}')
