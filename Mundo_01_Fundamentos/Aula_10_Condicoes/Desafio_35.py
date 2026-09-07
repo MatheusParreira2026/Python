743f5764728578182d2283fccb182c08ffbf6577
MatheusParreira2026 <matheusparreira.programacao@gmail.com>
2026-09-07 17:48:08 -0300

    Update Desafio_35.py

--- Mundo_01_Fundamentos/Aula_10_Condicoes/Desafio_35.py
+++ Mundo_01_Fundamentos/Aula_10_Condicoes/Desafio_35.py
@@ -65,9 +65,9 @@ print('-=' * 20)
 print('Analisador de Triângulos')
 print('-=' * 20)
 
-r1 = float(input('Primeiro segmento: '))
-r2 = float(input('Segundo segmento: '))
-r3 = float(input('Terceiro segmento: '))
+r1 = float(input(f'{cores['azul']}Primeiro segmento: '))
+r2 = float(input(f'{cores['azul']}Segundo segmento: '))
+r3 = float(input(f'{cores['azul']}Terceiro segmento: '))
 
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
     print(f'{cores['verde']}Os segmentos acima podem formar um triângulo!')
