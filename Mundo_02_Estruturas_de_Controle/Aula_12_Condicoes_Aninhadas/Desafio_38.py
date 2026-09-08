027cd1ec253c274e5925d7847105ec0452f1c106
MatheusParreira2026 <matheusparreira.programacao@gmail.com>
2026-09-08 16:29:09 -0300

    Novo

--- Mundo_02_Estruturas_de_Controle/Aula_12_Condicoes_Aninhadas/Desafio_38.py
+++ Mundo_02_Estruturas_de_Controle/Aula_12_Condicoes_Aninhadas/Desafio_38.py
@@ -2,3 +2,5 @@
 Desafio 38 (19:39): Leia dois números inteiros e compare-os, mostrando se o primeiro é maior,
 o segundo é maior ou se não existe valor maior (são iguais).
 '''
+
+
--- Mundo_02_Estruturas_de_Controle/Aula_12_Condicoes_Aninhadas/Desafio_43.py
+++ Mundo_02_Estruturas_de_Controle/Aula_12_Condicoes_Aninhadas/Desafio_43.py
@@ -1,6 +1,12 @@
 '''
-Desafio 43 (23:38): Leia o peso e a altura de uma pessoa, calcule o IMC e mostre o status: abaixo do peso,
-peso ideal, sobrepeso, obesidade ou obesidade mórbida.
+Desafio 43 (23:38): Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule o seu Índice
+de Massa Corpórea (IMC) e mostre o seu status, de acordo com a tabela abaixo:
+
+Abaixo de 18.5: Abaixo do Peso
+Entre 18.5 e 25: Peso ideal
+25 até 30: Sobrepeso
+30 até 40: Obesidade
+Acima de 40: Obesidade mórbida
 '''
 
 
