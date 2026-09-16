ad647b2724a21b9d1742e06f8c9ca510a67d5d52
MatheusParreira2026 <matheusparreira.programacao@gmail.com>
2026-09-16 17:39:46 -0300

    Update Desafio_39.py

--- Mundo_02_Estruturas_de_Controle/Aula_12_Condicoes_Aninhadas/Desafio_39.py
+++ Mundo_02_Estruturas_de_Controle/Aula_12_Condicoes_Aninhadas/Desafio_39.py
@@ -3,52 +3,75 @@ Desafio 39 (20:11): Leia o ano de nascimento de um jovem e informe se ele ainda 
 se é a hora exata ou se já passou do prazo. O programa deve mostrar quanto tempo falta ou quanto tempo passou
 do prazo.
 '''
+# from datetime import date
+#
+# cores = {
+#     # Reset
+#     'limpa': '\033[m',
+#
+#     # Cores normais
+#     'preto': '\033[30m',
+#     'vermelho': '\033[31m',
+#     'verde': '\033[32m',
+#     'amarelo': '\033[33m',
+#     'azul': '\033[34m',
+#     'magenta': '\033[35m',
+#     'ciano': '\033[36m',
+#     'branco': '\033[37m',
+#
+#     # Cores claras / brilhantes
+#     'cinza': '\033[90m',
+#     'vermelhoclaro': '\033[91m',
+#     'verdeclaro': '\033[92m',
+#     'amareloclaro': '\033[93m',
+#     'azulclaro': '\033[94m',
+#     'magentaclaro': '\033[95m',
+#     'cianoclaro': '\033[96m',
+#     'brancoclaro': '\033[97m',
+#
+#     # Estilos
+#     'negrito': '\033[1m',
+#     'fraco': '\033[2m',
+#     'sublinhado': '\033[4m',
+#     'piscando': '\033[5m',
+#     'invertido': '\033[7m',
+#
+#     # Combinações
+#     'pretoebranco': '\033[7;97m',
+# }
+#
+# ano_de_nascimento = int(input('Digite o ano em que você nasceu: '))
+#
+# idade = date.today().year - ano_de_nascimento
+#
+# if idade == 18:
+#     print(f'Você tem {cores['verde']}{idade}{cores['verde']} anos{cores['limpa']} de idade, portanto deve se alistar.')
+# elif idade > 18:
+#     prazo = idade - 18
+#     print(f'Você tem {cores['verde']}{idade} anos{cores['limpa']} de idade, sendo assim se passaram {prazo} anos para efetuar o alistamento.')
+# else:
+#     prazo = 18 - idade
+#     print(f'Você tem {cores['verde']}{idade} ano(s){cores['limpa']} de idade, você deve esperar {prazo} anos para se alistar.')
+
+'''
+Alternativa feita pelo professor
+'''
+
 from datetime import date
 
-cores = {
-    # Reset
-    'limpa': '\033[m',
+ano_atual = date.today().year
+nascimento = int(input('Ano de nascimento: '))
+idade = ano_atual - nascimento
 
-    # Cores normais
-    'preto': '\033[30m',
-    'vermelho': '\033[31m',
-    'verde': '\033[32m',
-    'amarelo': '\033[33m',
-    'azul': '\033[34m',
-    'magenta': '\033[35m',
-    'ciano': '\033[36m',
-    'branco': '\033[37m',
-
-    # Cores claras / brilhantes
-    'cinza': '\033[90m',
-    'vermelhoclaro': '\033[91m',
-    'verdeclaro': '\033[92m',
-    'amareloclaro': '\033[93m',
-    'azulclaro': '\033[94m',
-    'magentaclaro': '\033[95m',
-    'cianoclaro': '\033[96m',
-    'brancoclaro': '\033[97m',
-
-    # Estilos
-    'negrito': '\033[1m',
-    'fraco': '\033[2m',
-    'sublinhado': '\033[4m',
-    'piscando': '\033[5m',
-    'invertido': '\033[7m',
-
-    # Combinações
-    'pretoebranco': '\033[7;97m',
-}
-
-ano_de_nascimento = int(input('Digite o ano em que você nasceu: '))
-
-idade = date.today().year - ano_de_nascimento
+print(f'Quem nasceu em {nascimento} tem {idade} anos em {ano_atual}.')
 
 if idade == 18:
-    print(f'Você tem {cores['verde']}{idade}{cores['verde']} anos{cores['limpa']} de idade, portanto deve se alistar.')
-elif idade > 18:
-    prazo = idade - 18
-    print(f'Você tem {cores['verde']}{idade} anos{cores['limpa']} de idade, sendo assim se passaram {prazo} anos para efetuar o alistamento.')
+    print('Você tem que se alistar imediatamente!')
+elif idade < 18:
+    saldo = 18 - idade
+    print(f'Ainda faltam {saldo} anos para o alistamento.')
+    print(f'Seu alistamento será em {ano_atual + saldo}.')
 else:
-    prazo = 18 - idade
-    print(f'Você tem {cores['verde']}{idade} ano(s){cores['limpa']} de idade, você deve esperar {prazo} anos para se alistar.')
+    saldo = idade - 18
+    print(f'Você já deveria ter se alistado há {saldo} anos.')
+    print(f'Seu alistamento foi em {ano_atual - saldo}.')
