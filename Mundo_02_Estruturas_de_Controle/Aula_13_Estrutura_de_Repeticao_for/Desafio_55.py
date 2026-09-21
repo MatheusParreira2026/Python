e459896b30201254d42de7ef449dfff9d6da5b57
MatheusParreira2026 <matheusparreira.programacao@gmail.com>
2026-09-21 15:57:55 -0300

    Novo

--- Mundo_02_Estruturas_de_Controle/Aula_13_Estrutura_de_Repeticao_for/Desafio_55.py
+++ Mundo_02_Estruturas_de_Controle/Aula_13_Estrutura_de_Repeticao_for/Desafio_55.py
@@ -5,4 +5,39 @@ Faça um programa que leia o peso de cinco pessoas.
 No final, mostre qual foi o maior e o menor peso lidos.
 '''
 
+cores = {
+    # Reset
+    'limpa': '\033[m',
+
+    # Cores normais
+    'preto': '\033[30m',
+    'vermelho': '\033[31m',
+    'verde': '\033[32m',
+    'amarelo': '\033[33m',
+    'azul': '\033[34m',
+    'magenta': '\033[35m',
+    'ciano': '\033[36m',
+    'branco': '\033[37m',
+
+    # Cores claras / brilhantes
+    'cinza': '\033[90m',
+    'vermelhoclaro': '\033[91m',
+    'verdeclaro': '\033[92m',
+    'amareloclaro': '\033[93m',
+    'azulclaro': '\033[94m',
+    'magentaclaro': '\033[95m',
+    'cianoclaro': '\033[96m',
+    'brancoclaro': '\033[97m',
+
+    # Estilos
+    'negrito': '\033[1m',
+    'fraco': '\033[2m',
+    'sublinhado': '\033[4m',
+    'piscando': '\033[5m',
+    'invertido': '\033[7m',
+
+    # Combinações
+    'pretoebranco': '\033[7;97m',
+}
+
 
