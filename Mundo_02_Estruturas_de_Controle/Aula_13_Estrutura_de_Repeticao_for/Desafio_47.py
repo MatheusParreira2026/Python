96cc57ed4a4f63f3043e872bcbd80f82d622a96b
MatheusParreira2026 <matheusparreira.programacao@gmail.com>
2026-09-21 16:44:20 -0300

    Novo

--- Mundo_02_Estruturas_de_Controle/Aula_13_Estrutura_de_Repeticao_for/Desafio_47.py
+++ Mundo_02_Estruturas_de_Controle/Aula_13_Estrutura_de_Repeticao_for/Desafio_47.py
@@ -4,7 +4,6 @@ DESAFIO 047
 Crie um programa que mostre na tela todos os números
  pares que estão no intervalo entre 1 e 50.
 '''
-
 cores = {
     # Reset
     'limpa': '\033[m',
@@ -40,4 +39,12 @@ cores = {
     'pretoebranco': '\033[7;97m',
 }
 
+# for n in range(1, 51):
+#     if n % 2 == 0:
+#         print(n)
 
+'''
+Alternativa 01
+'''
+for n in range(2, 51, 2):
+    print(n)
--- Mundo_02_Estruturas_de_Controle/Aula_13_Estrutura_de_Repeticao_for/Desafio_48.py
+++ Mundo_02_Estruturas_de_Controle/Aula_13_Estrutura_de_Repeticao_for/Desafio_48.py
@@ -41,4 +41,8 @@ cores = {
     'pretoebranco': '\033[7;97m',
 }
 
-
+soma = 0
+for n in range(1, 501):
+    if n % 3 == 0 and n % 2 != 0:
+        soma += n
+print(soma)
